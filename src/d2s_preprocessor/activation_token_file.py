bf5a2f2c81d61dfb5c3ad3e0b78e8e2cd36a9bef
jp-sft <kvnjessy@gmail.com>
2024-04-07 08:58:46 +0100

    Refactor

--- src/d2s_preprocessor/activation_token_file.py
+++ src/d2s_preprocessor/activation_token_file.py
@@ -6,24 +6,19 @@ DATA_DIR = pathlib.Path(__file__).parent / "data"
 def get_vat_act_token_file_path():
     res = DATA_DIR / "activation_token" / "vat_activation_token.csv"
     if not res.exists():
-        raise FileNotFoundError(
-            f"File Token for VAT not found: {res}"
-        )
+        raise FileNotFoundError(f"File Token for VAT not found: {res}")
     return res
 
 
 def get_invoice_number_token_file_path():
     res = DATA_DIR / "activation_token" / "invoice_number_token.csv"
     if not res.exists():
-        raise FileNotFoundError(
-            f"File Token for Invoice Number not found {res}"
-        )
+        raise FileNotFoundError(f"File Token for Invoice Number not found {res}")
     return res
 
 
 def get_amounts_activation_token_path():
     res = DATA_DIR / "activation_token" / "amounts_activation_token.csv"
-    raise FileNotFoundError(
-        f"File Token for Amounts not found: {res}"
-    )
+    if not res.exists():
+        raise FileNotFoundError(f"File Token for Amounts not found: {res}")
     return res
